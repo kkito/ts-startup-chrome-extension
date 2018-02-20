764b88253e5fd0e43aff70132af404e7515bfede
kkito <kkito@sohu.com>
2018-02-20 19:04:44 +0800

    greeting demo

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ declare const chrome: {
   };
 };
 
+import { Greeter } from './greeter';
+
 function changeBackgroundColor(color: string) {
   const script = 'document.body.style.backgroundColor="' + color + '";';
   chrome.tabs.executeScript({
@@ -13,10 +15,15 @@ function changeBackgroundColor(color: string) {
 
 document.addEventListener('DOMContentLoaded', () => {
   const dropdown = document.getElementById('dropdown');
+  const greetingSpan = document.getElementById('greeting');
 
   if (dropdown) {
     dropdown.addEventListener('change', () => {
       changeBackgroundColor((dropdown as any).value);
     });
+  }
+
+  if (greetingSpan) {
+    greetingSpan.innerHTML = new Greeter('kk').greet();
   }
 });
