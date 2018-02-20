2da9a373ef8ff9dba1df123ec8253c9ecf12c2ed
kkito <kkito@sohu.com>
2018-02-20 18:37:30 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    "index": './src/index.ts',
+    "index.min": './src/index.ts',
+  },
+  output: {
+    filename: 'dist/[name].js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader' },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'babel-loader!ts-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    }) ,
+    new CopyWebpackPlugin([
+      { from: 'src/hello.html', to: 'dist/' },
+      { from: 'src/hello_extensions.png', to: 'dist/' },
+      { from: 'src/manifest.json', to: 'dist/' }
+    ])
+  ]
+}
